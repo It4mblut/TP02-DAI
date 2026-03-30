1051f7dfbf465c2e501b6b0f9d51d86e4c778953
Android Studio <devandroid@ort>
2026-03-30 10:24:16 -0300

    Actualizando el modulo Matematica a la nueva consigna

--- scr/modules/matematica.js
+++ scr/modules/matematica.js
@@ -1,6 +1,10 @@
-//que tenga 4 funciones (dos de ellas arrow functions) y una constante (PI). Exportarlas y utilizarlas en el programa principal.
+//que tenga 4 funciones sumar, restar, multiplicar y dividir (dos de ellas arrow functions), 
+// una constante (PI) y un Array con 4 string que tengan este contenido ["dos", "cuatro", "ocho", "diez"].
+// Exportarlas y utilizarlas en el programa principal. en el caso del array, mostrar todos sus elementos.
 
 const pi = 3.14
+
+let arrayNumeros = ["dos", "cuatro", "ocho", "diez"];
 
 const sumar = (a,b) => a + b;
 
@@ -10,4 +14,6 @@ const multiplicar = (a,b) => a * b;
 
 const dividir = (a,b) => a / b;
 
-export {pi, sumar, restar, multiplicar, dividir}
+
+
+export {pi, arrayNumeros, sumar, restar, multiplicar, dividir}
