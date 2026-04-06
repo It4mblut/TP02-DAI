cac8ffb453190a54fbe99609faaaf9924d40d5f8
EITAN URIEL KOZE <49433681@ort>
2026-04-06 09:45:07 -0300

    Anotaciones sobre Fs en ej4

--- scr/EJ04.js
+++ scr/EJ04.js
@@ -1,13 +1,14 @@
 import fs from 'fs';
 
-const ARCHIVO_ENTRADA           = "./archivo-entrada.txt";
+const ARCHIVO_ENTRADA           = "./archivo-entrada.txt"; //Me refiero a cada archivo mediante variables diferentes
 const ARCHIVO_SALIDA            = "./archivo-salida.txt";
-console.clear();
 
-copiar(ARCHIVO_ENTRADA, ARCHIVO_SALIDA);
+
+console.clear();
+copiar(ARCHIVO_ENTRADA, ARCHIVO_SALIDA); //Defino cual archivo es origen y cual destino
 
 function copiar(origen, destino){
-const contenidoArchivo = fs.readFileSync(origen, 'utf-8')
-fs.writeFileSync(destino, contenidoArchivo)
+const contenidoArchivo = fs.readFileSync(origen, 'utf-8') //Leo el archivo de origen y guardo el contenido en "contenidoArchivo"
+fs.writeFileSync(destino, contenidoArchivo)                 //Escribo en el archivo destino 
 
 }
--- scr/EJ05.js
+++ scr/EJ05.js
@@ -1,11 +1,8 @@
 let miUrl    = null;
-
 let miObjeto = null;
 
 miUrl    = 'http://www.ort.edu.ar:8080/alumnos/index.htm?curso=2022&mes=mayo';
-
 miObjeto = parsearUrl (miUrl);
-
 console.log(miObjeto);
 
 
