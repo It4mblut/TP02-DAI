9a03ed43c6c28dfe7212374e00d1b6e25c8da210
EITAN URIEL KOZE <49433681@ort>
2026-04-06 09:58:29 -0300

    Ej5 terminado y en funcionamiento (+ anotaciones)

--- scr/EJ05.js
+++ scr/EJ05.js
@@ -1,13 +1,19 @@
+import { URL } from 'url';
+
 let miUrl    = null;
 let miObjeto = null;
+
+function parsearUrl(laURL){ //Ojo, para tener en cuenta a futuro, tuve que mover la funcion acá arriba (antes de miUrl y etc) para que me tome los valores (particularmente el de miObjeto)
+    const url = new URL(laURL);
+    return {
+        host: url.protocol + '//' + url.host,
+        pathname: url.pathname,
+        parametros: url.searchParams
+    }
+}
 
 miUrl    = 'http://www.ort.edu.ar:8080/alumnos/index.htm?curso=2022&mes=mayo';
 miObjeto = parsearUrl (miUrl);
 console.log(miObjeto);
 
 
-function parsearUrl(laURL){
-
-    // No seas vago, acá hay que hacer el cuerpo de la función!!!
-
-}
